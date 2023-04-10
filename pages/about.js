5f45aab6d3fdea3df4d7a8025551685f55e14411
Tone Lopes Monte <119428578+ToneLopes@users.noreply.github.com>
2023-04-10 16:26:46 -0300

    correcoes do projeto

--- pages/about.js
+++ pages/about.js
@@ -5,18 +5,48 @@ export default function About() {
   return (
     <div className={styles.about}>
       <h1>Sobre o Projeto</h1>
-      <p>
-        {" "}
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s,{" "}
-      </p>
       <Image
         src="/images/charizard.png"
         width={300}
         height={300}
         alt="Charizard"
       />
+      <p>
+        O projeto que desenvolvi é um site que utiliza a <strong>API do Pokemon</strong> para
+        mostrar informações sobre os Pokemons. <strong>
+          Ele foi criado com a tecnologia
+          Next.js no React.js
+        </strong>, seguindo o curso do <strong>
+          professor Matheus Batisti do
+          canal "Hora de Codar" no YouTube.
+        </strong>
+      </p>
+      <p>
+        O site apresenta uma lista de todos <strong>
+          os Pokemons disponíveis na API, com
+          suas respectivas imagens, nomes e tipos.
+        </strong>
+      </p>
+      <p>
+        Ao clicar em um Pokemon, o usuário é direcionado para uma página com
+        informações mais detalhadas sobre ele, <strong>
+          incluindo sua descrição,
+          habilidades, estatísticas e evoluções
+        </strong>. Também é possível ver outras
+        informações relacionadas, como quais Pokemons podem ser encontrados na
+        mesma região.
+      </p>
+      
+      <p>
+        O site é responsivo e foi desenvolvido seguindo <strong>
+          boas práticas de
+          programação, com um código limpo e bem estruturado.
+        </strong>
+      </p>
+      <p>
+        Foi uma ótima oportunidade para <strong>aprender sobre Next.js e React.</strong>js, além
+        de aprimorar minhas habilidades em <strong>Desenvolvimento Web</strong>.
+      </p>
     </div>
   );
 }
--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -37,35 +37,36 @@ export const getStaticProps = async (context) => {
 export default function Pokemon({ pokemon }) {
   return (
     <div className={styles.pokemon_container}>
-      <h1 className={styles.title}>{pokemon.name}</h1>
-      <Image
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
-        width={250}
-        height={250}
-        alt={pokemon.name}
-      />
-
-      <div>
-        <h3>Número:</h3>
-        <p>#{pokemon.id}</p>
-      </div>
-      <div>
-        <h3>Tipo:</h3>
-        <div className={styles.types_container}>
-          {pokemon.types.map((item, index) => (
-            <span key={index} className={`${styles.type} ${styles['type_'+ item.type.name]}`} >{item.type.name}</span>
-          ))}
-        </div>
-      </div>
-      <div className={styles.data_container}>
-        <div className={styles.data_height}>
-          <h4>Altura:</h4>
-          <p>{pokemon.height * 10} cm</p>
-        </div>
-        <div className={styles.data_weight}>
-          <h4>Peso:</h4>
-          <p>{pokemon.weight / 10} Kg</p>
-        </div>
+      <div className={styles.pokemon_container_card}>
+          <h1 className={styles.title}>{pokemon.name}</h1>
+          <Image
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+            width={250}
+            height={250}
+            alt={pokemon.name}
+          />
+          <div>
+            <h3>Número:</h3>
+            <p>#{pokemon.id}</p>
+          </div>
+          <div>
+            <h3>Tipo:</h3>
+            <div className={styles.types_container}>
+              {pokemon.types.map((item, index) => (
+                <span key={index} className={`${styles.type} ${styles['type_'+ item.type.name]}`} >{item.type.name}</span>
+              ))}
+            </div>
+          </div>
+          <div className={styles.data_container}>
+            <div className={styles.data_height}>
+              <h4>Altura:</h4>
+              <p>{pokemon.height * 10} cm</p>
+            </div>
+            <div className={styles.data_weight}>
+              <h4>Peso:</h4>
+              <p>{pokemon.weight / 10} Kg</p>
+            </div>
+          </div>
       </div>
     </div>
   );
