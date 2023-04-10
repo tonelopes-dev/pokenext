0aabead36a01fbd8dee68988d7099178d2cb5786
Tone Lopes Monte <119428578+ToneLopes@users.noreply.github.com>
2023-04-10 20:27:17 -0300

    correcao da margen da navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,24 +4,26 @@ import styles from '../styles/Navbar.module.css'
 
 export default function NavBar() {
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.logo}>
-        <Image
-          src="/images/pokeball.png"
-          width="30"
-          height="30"
-          alt="PokeNext"
-        />
-        <h1>PokeNext</h1>
-      </div>
-      <ul className={styles.link_items}>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">Sobre</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className={styles.navbar_container}>
+      <nav className={styles.navbar}>
+        <div className={styles.logo}>
+          <Image
+            src="/images/pokeball.png"
+            width="30"
+            height="30"
+            alt="PokeNext"
+          />
+          <h1>PokeNext</h1>
+        </div>
+        <ul className={styles.link_items}>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">Sobre</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
